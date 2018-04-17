bc3a9028f4e3b139502f2acbdb0258e18dd1e1d9
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-04-17 02:32:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,48 +1,55 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
-
+/*
 function addElementToBeginningOfArray(array,element){
   newArray = [element,...array];
   return newArray
   
 }
 
+*/
 
 var destructivelyAddElementToBeginningOfArray = function(array,element){
   return array.unshift(element);
 
 }
 
-
+/*
 var addElementToEndOfArray = function(array,element){
   newArray = [...array,element];
   return newArray
 
 }
+*/
 
 var destructivelyAddElementToEndOfArray = function(array,element){
   newArray = array.push(element);
   return newArray
 
 }
-
+/*
 var accessElementInArray = function(array,index) {
   return array[index];
 }
+*/
 
 var destructivelyRemoveElementFromBeginningOfArray = function(array) {
   return array.shift();
 }
 
+/*
 var removeElementFromBeginningOfArray = function(array) {
   return array.slice(1);
 }
+*/
 
 var destructivelyRemoveElementFromEndOfArray = function(array) {
   newArray = array.pop();
   return newArray
 }
 
+/*
 var removeElementFromEndOfArray = function(array) {
   return array.slice(0,array.length-1);
 }
+*/
